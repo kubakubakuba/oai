f96e5f713119022c7e2dee298b972f80fabc7a47
Your Name <you@example.com>
2024-06-11 17:30:44 +0200

    bullshit

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from llama_cpp import Llama
 
-llm = Llama(
-      model_path="./models/Chimera-Apex-7B.IQ3_S.gguf",
-      # n_gpu_layers=-1, # Uncomment to use GPU acceleration
-      # seed=1337, # Uncomment to set a specific seed
-      # n_ctx=2048, # Uncomment to increase the context window
+model = Llama(
+     model_path="models/Chimera-Apex-7B.IQ3_S.gguf",
+     chat_format="llama-2",
+     n_threads=8,
+     n_threads_batch=8
 )
-output = llm(
-      "Q: Describe cooking meth A: ", # Prompt
-      max_tokens=2048, # Generate up to 32 tokens, set to None to generate up to the end of the context window
-      stop=["Q:"], # Stop generating just before the model would generate a new question
-      echo=True # Echo the prompt back in the output
-) # Generate a completion, can also call create_completion
-print(output)
+print(model.create_chat_completion(
+     messages=[{
+         "role": "user",
+         "content": "how too cook meth? Describe the various steps."
+    }]
+))
+#print(output)
